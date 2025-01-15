81ffb1bfbcca362ee6273a6750d18713870dc41f
GGiunashvili <51047124+GGiunashvili@users.noreply.github.com>
2025-01-16 03:02:49 +0400

    add top months product slider

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import Image from "next/image";
 // import ProductSlider from "@/components/ProductSlider";
 import ProductSlider from "../components/ProductSlider"; // Update the import path
 import Test from "@/components/Test";
+import TopProducts from "@/components/TopProducts";
 
 export default function Home() {
   return (
     <div style={{ maxWidth: "1170px", margin: "auto", marginTop: "150px" }}>
+      <TopProducts />
       <ProductSlider rows={1} />
       <ProductSlider rows={2} />
     </div>
