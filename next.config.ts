21ef619d1b57d52ad697279e0178ad823f8757fa
seriojaa <kaliashvilisergi@gmail.com>
2025-02-20 21:44:04 +0400

    update next.config.js to allow external images from multiple sources

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: [
+      "f00.esfr.pl",
+      "s3-alpha-sig.figma.com",
+      "cdn.salla.sa",
+      "prod-api.mediaexpert.pl",
+      "cdn.trixie.de", // Add this domain
+    ],
+  },
 };
 
 export default nextConfig;
